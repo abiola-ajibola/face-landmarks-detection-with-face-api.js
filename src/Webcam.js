d9347103dcab2f48b522b5323b6a89d470de0c10
Abiola Ajibola <ajibolaabiolaahmed@gmail.com>
2021-02-20 15:42:19 +0100

    detecting face landmarks using canvas instead of CSS

--- src/Webcam.js
+++ src/Webcam.js
@@ -1,50 +1,68 @@
 import Webcam from 'react-webcam'
 import { useRef, useEffect } from 'react'
 import * as faceapi from 'face-api.js';
+import { DrawFaceLandmarks } from 'face-api.js/build/commonjs/draw';
+
 
 function Livecam() {
     const camera = useRef()
     async function loadModels() {
         console.log("loading models")
-        Promise.all([
+        return await Promise.all([
             faceapi.nets.faceLandmark68TinyNet.loadFromUri("/models"),
             faceapi.nets.tinyFaceDetector.loadFromUri("./models")
         ]).then(() => console.log("models loaded"))
     }
 
     async function detectFaces() {
-
         if (camera && camera.current && camera.current.video) {
-            const { video } = camera.current
-            const results = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks(true)
-            const landmarks = results.length && results[0].landmarks
-
-            if (document.body.contains(document.querySelector(".dot"))) {
-                document.querySelectorAll(".dot").forEach(point => point.remove())
-            }
-            if (landmarks) {
-                landmarks.positions.map((point) => {
-                    const dot = document.createElement("span")
-                    const { _x, _y } = point
-                    dot.classList.add("dot")
-                    if (dot) {
-                        dot.style.top = _y + "px"
-                        dot.style.left = _x + "px"
-                        document.querySelector("#cam-wrapper").append(dot)
-                    }
-                })
+            console.log("inside if block")
+            const { video } = camera.current;
+            // (async () => {
+            video.onloadedmetadata = () => {
+                const canvas = faceapi.createCanvasFromMedia(video)
+                document.querySelector("#cam-wrapper").append(canvas)
+                const displaySize = { width: video.clientWidth, height: video.clientHeight }
+                faceapi.matchDimensions(canvas, displaySize)
+                console.log("before if")
+                if (canvas) {
+                    console.log("after if")
+                    setInterval(async () => {
+                        const results = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions({})).withFaceLandmarks(true)
+                        const resized = faceapi.resizeResults(results, displaySize)
+                        // DrawFaceLandmarks()
+                        let options = {
+                            drawLines: true,
+                            drawPoints: false,
+                            lineWidth: 1,
+                            lineColor: "#00dd00",
+                        }
+                        
+                        /**
+                         DrawFaceLandmarksOptions({
+                            drawLines: true,
+                            drawPoints: false,
+                            lineWidth: 1,
+                            lineColor: "#00dd00",
+                        })
+                         */
+                        
+                        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+                        console.log(resized)
+                        faceapi.draw.drawFaceLandmarks(canvas, resized)
+                    }, 1000)
+                }
             }
         }
     }
 
-    useEffect(async () => {
-        console.log("effect")
-        await loadModels()
+    useEffect(() => {
+        (async () => {
+            console.log("effect")
+            await loadModels()
+            await detectFaces()
+        })()
     })
-
-    setInterval(async () => {
-        await detectFaces()
-    }, 1000)
 
     return (
         <div id="cam-wrapper">
@@ -53,7 +71,6 @@ function Livecam() {
                 audio={false}
             />
         </div>
-
     )
 }
 
